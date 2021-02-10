83dd2cf25089311c725db5ad9a660b330208a17f
mahi <mohiddinoshim@yahoo.com>
2021-02-10 14:37:41 +0600

    Add listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :listings
   get 'pages/about'
   get 'pages/contact'
 
-  root 'pages#index'
+  root 'listings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
