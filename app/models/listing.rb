2aa37b3520b14d3c07aad2cb5edcbcfe4a51b1eb
mahi <mohiddinoshim@yahoo.com>
2021-02-17 17:46:23 +0600

    Validate User and Listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,7 @@
 class Listing < ApplicationRecord
 	mount_uploader :image, ImageUploader
+
+	validates :name, :description, :price, presence: true
+	validates :price, numericality: { greater_then: 0 }
+	validates_absence_of :image
 end
